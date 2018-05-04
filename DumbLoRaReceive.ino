83a661d4a47cb65b560498725081984bfc69617f
Patrice LACOUTURE <p.lacouture@see.fr>
2018-05-04 10:29:03 +0200

    Initial Commit using branch dev/lacouture/MKRWAN-1300 of arduino-LoRa

---
+++ DumbLoRaReceive.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <MKRWAN.h>
+
+LoRaModem modem;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  modem.dumb();
+
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(LORA_IRQ_DUMB, 6, 1); // set CS, reset, IRQ pin
+  LoRa.setSPIFrequency(100000);
+
+  if (!LoRa.begin(&SPI1, 868E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  
+  LoRa.setSpreadingFactor(12);
+  //LoRa.setSignalBandwidth(31.25E3);
+  LoRa.enableCrc();
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}
+
